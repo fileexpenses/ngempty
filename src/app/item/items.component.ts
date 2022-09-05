61c19c80ebbe116790e056d788334cbf6eeb1513
Gerard Daly <info@fileexpenses.com>
2022-09-05 08:55:48 +0100

    Put date picker back in

--- src/app/item/items.component.ts
+++ src/app/item/items.component.ts
@@ -1,6 +1,6 @@
 import {Component, Injectable, OnInit, ViewContainerRef, ViewChild, ElementRef} from "@angular/core";
 import { EventData, ScrollView, SwipeGestureEventData } from '@nativescript/core';
-
+import * as ModalPicker from 'nativescript-modal-datetimepicker';
 
 
 @Component({
@@ -14,7 +14,7 @@ import { EventData, ScrollView, SwipeGestureEventData } from '@nativescript/core
 @Injectable()
 export class ItemsComponent implements OnInit {
 
-
+  public selectedDate: string;
 
   @ViewChild("scrollView", { static: false }) scrollView: ElementRef;
 
@@ -27,7 +27,42 @@ export class ItemsComponent implements OnInit {
   onCreate() {
   }
 
+  zeroFill( number, width ) {
+    width -= number.toString().length;
+    if ( width > 0 ) {
+      return new Array( width + (/\./.test( number ) ? 2 : 1) ).join( '0' ) + number;
+    }
+    return number + ""; // always return a string
+  }  
+
+  procDate(result) {
+      var day="";
+      var month="";
+      if (result.day<10) {
+        day=this.zeroFill(result.day, 2);
+      } else {
+        day=String(result.day);
+      }
+      if (result.month<10) {
+        month=this.zeroFill(result.month, 2);
+      } else {
+        month=String(result.month);
+      }
+      let year=result.year;
+      let proper_date=year+"-"+month+"-"+day;
+      return proper_date;
+  }
+
   getDate() {
+    const picker = new ModalPicker.ModalDatetimepicker();
+    picker.pickDate({
+      maxDate: new Date(),
+    }).then((result) => {
+      let testDate=this.procDate(result);
+      this.selectedDate=testDate;
+    }).catch((error) => {
+      console.log('Error: ' + error);
+    });
   }
 
   onTap() {
